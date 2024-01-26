86485ee20188c9f8668d60e0431725ef569d6f88
nsr <githubnsr@gmail.com>
2024-01-26 22:36:45 +0530

    Added Face mesh

--- src/Translate.jsx
+++ src/Translate.jsx
@@ -1,5 +1,5 @@
-import {HandLandmarker,FilesetResolvers}from '@mediapipe/tasks-vision'
-import React, { useRef} from 'react';
+import {HandLandmarker,FilesetResolver, FaceLandmarker, DrawingUtils, /*PoseLandmarker*/}from '@mediapipe/tasks-vision'
+import React, {useRef} from 'react';
 import Webcam from 'react-webcam';
 
 
@@ -22,6 +22,8 @@ const Translate = () => {
   
 
     let handLandmarker='undefined';
+    let faceLandmarker='undefined';
+    // let poseLandmarker='undefined';
     const createHandLandmarker = async () => {
       const vision = await FilesetResolver.forVisionTasks(
         "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm"
@@ -34,18 +36,40 @@ const Translate = () => {
         runningMode: 'VIDEO',
         numHands: 2
       });
-      
-      var intervalid=setInterval(()=>{
+
+      faceLandmarker = await FaceLandmarker.createFromOptions(vision, {
+        baseOptions: {
+          modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task`,
+          delegate: "GPU"
+        },
+        outputFaceBlendshapes: true,
+        runningMode:'VIDEO',
+        numFaces: 1
+      });
+      // poseLandmarker = await PoseLandmarker.createFromOptions(vision, {
+      //   baseOptions: {
+      //     modelAssetPath: `https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/1/pose_landmarker_lite.task`,
+      //     delegate: "GPU"
+      //   },
+      //   runningMode: 'VIDEO',
+      //   numPoses: 2
+      // });
+
+      setInterval(()=>{
         handDetector(handLandmarker)
+        faceDetector(faceLandmarker)
+        // poseDetector(poseLandmarker)
       },100)
-      if (wbcam==='true'){
-        clearInterval(intervalid)
-      }
-    };
+    }
+      
+      
     
 
   const drawLandmarks = (landmarksArray) => {
+      canvasRef.current.width=webcamRef.current.video.videoWidth
+      canvasRef.current.height=webcamRef.current.video.videoHeight
       const canvas = canvasRef.current;
+      
       const ctx=canvas.getContext('2d');
       ctx.clearRect(0, 0, canvas.width, canvas.height);
       ctx.fillStyle = 'white';  
@@ -68,8 +92,59 @@ const Translate = () => {
       });
   };
   
+  const drawfaceLandmarks=(landmarksArray)=>{
+  const ctx = canvasRef.current.getContext("2d");
+  const drawingUtils = new DrawingUtils(ctx);
+  for (const landmarks of landmarksArray) {
+    drawingUtils.drawConnectors(
+      landmarks,
+      FaceLandmarker.FACE_LANDMARKS_TESSELATION,
+      { color: "#C0C0C070", lineWidth: 1 }
+    );
+    drawingUtils.drawConnectors(
+      landmarks,
+      FaceLandmarker.FACE_LANDMARKS_RIGHT_EYE,
+      { color: "#FF3030" }
+    );
+    drawingUtils.drawConnectors(
+      landmarks,
+      FaceLandmarker.FACE_LANDMARKS_RIGHT_EYEBROW,
+      { color: "#FF3030" }
+    );
+    drawingUtils.drawConnectors(
+      landmarks,
+      FaceLandmarker.FACE_LANDMARKS_LEFT_EYE,
+      { color: "#30FF30" }
+    );
+    drawingUtils.drawConnectors(
+      landmarks,
+      FaceLandmarker.FACE_LANDMARKS_LEFT_EYEBROW,
+      { color: "#30FF30" }
+    );
+    drawingUtils.drawConnectors(
+      landmarks,
+      FaceLandmarker.FACE_LANDMARKS_FACE_OVAL,
+      { color: "#E0E0E0" }
+    );
+    drawingUtils.drawConnectors(landmarks, FaceLandmarker.FACE_LANDMARKS_LIPS, {
+      color: "#E0E0E0"
+    });
+    drawingUtils.drawConnectors(
+      landmarks,
+      FaceLandmarker.FACE_LANDMARKS_RIGHT_IRIS,
+      { color: "#FF3030" }
+    );
+    drawingUtils.drawConnectors(
+      landmarks,
+      FaceLandmarker.FACE_LANDMARKS_LEFT_IRIS,
+      { color: "#30FF30" }
+    );
+  }}
+    
 
-    const handDetector = async (handLandmarker)=>{
+
+
+const handDetector = async (handLandmarker)=>{
       if (
         typeof webcamRef.current!=='undefined'&& webcamRef.current !== null && webcamRef.current.video.readyState===4){
           const video=webcamRef.current.video;
@@ -85,25 +160,63 @@ const Translate = () => {
           drawLandmarks(hand.landmarks)
 
         } 
-    }
+    };
+    const faceDetector = async (faceLandmarker)=>{
+      if (
+        typeof webcamRef.current!=='undefined'&& webcamRef.current !== null && webcamRef.current.video.readyState===4){
+          const video=webcamRef.current.video;
+          const videoWidth=webcamRef.current.video.videoWidth;
+          const videoHeight=webcamRef.current.video.videoHeight;
 
-    
+          webcamRef.current.video.width=videoWidth;
+          webcamRef.current.video.height=videoHeight;
 
+          canvasRef.current.width=videoWidth;
+          canvasRef.current.height=videoHeight;
 
+          const face= await faceLandmarker.detectForVideo(video, performance.now());
 
+          drawfaceLandmarks(face.faceLandmarks)
+        } 
+    };
+    // const poseDetector = async (poseLandmarker)=>{
+    //   if (
+    //     typeof webcamRef.current!=='undefined'&& webcamRef.current !== null && webcamRef.current.video.readyState===4){
+    //       const video=webcamRef.current.video;
+    //       const videoWidth=webcamRef.current.video.videoWidth;
+    //       const videoHeight=webcamRef.current.video.videoHeight;
+
+    //       webcamRef.current.video.width=videoWidth;
+    //       webcamRef.current.video.height=videoHeight;
+
+    //       canvasRef.current.width=videoWidth;
+    //       canvasRef.current.height=videoHeight;
+    //       const ctx = canvasRef.current.getContext("2d");
+    //       const drawingUtils = new DrawingUtils(ctx);
+    //       await poseLandmarker.detectForVideo(video, performance.now(), (result) => {
+    //         ctx.save();
+    //         ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+    //         for (const landmark of result.landmarks) {
+    //           drawingUtils.drawLandmarks(landmark, {
+    //             radius: (data) => DrawingUtils.lerp(data.from|data.z, -0.15, 0.1, 5, 1)
+    //           });
+    //           drawingUtils.drawConnectors(landmark, PoseLandmarker.POSE_CONNECTIONS);
+    //         }
+    //         ctx.restore();
+    //       });
+
+    //     } 
+    // };
     return( 
     <>
     <div className="camera">
     <Webcam ref={webcamRef} style={camStyle}/>
     <canvas ref={canvasRef} style={camStyle}/>
-    <button onClick={()=>{
-      createHandLandmarker();
-    }}>Start Detection</button>
-
+    <button onClick={()=>{createHandLandmarker();}}>Start Detection</button>
     </div>
     </>
     );
-}
+};
 
 export default Translate;
 
